1fb09725d13d325c92df70dad3ce1d359f053ead
akhilThePower <akhilraj7685@gmail.com>
2024-05-18 16:00:21 +0530

    dashboard added

--- projects/tracking-webapp/src/app/dashboard/dashboard.component.ts
+++ projects/tracking-webapp/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+
+
+  chartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
+  ];
+  chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  chartOptions = {
+    responsive: true,
+  };
+  chartColors = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    }
+  ];
+  chartLegend = true;
+  chartType = 'line';
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
